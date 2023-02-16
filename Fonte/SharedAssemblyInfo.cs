770c1bc9016015ad4119015ae6c54bdc217c98ca
tatohonma <milton@pdvseven.com.br>
2023-02-16 13:22:24 -0300

    ajuste versao

--- Fonte/SharedAssemblyInfo.cs
+++ Fonte/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("PDV7")]
 [assembly: AssemblyCopyright("Copyright © PDVSeven 2014-2022")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.24.5.0")]
+[assembly: AssemblyVersion("2.24.5.1")]
 
 /* CheckList antes de gerar Release!
  *    Verificar se os WS2 (Autoatendimento, Saida, PainelMesaComanda) estão apontando para o "." que é para descoberta automática via UDP
@@ -66,6 +66,10 @@ Lista das últimas versões e FIX
 
 ===============================================================================
 ===============================================================================
+
+2.24.5.1 ======================================================================
+    Correção: Correção do cancelamento automático do cupom
+        A identificação do cupom é feita usando o GUIDMovimentacao
 
 2.24.5.0 ======================================================================
     Melhoria: Otimização tela de cancelamento de cupom fiscal
