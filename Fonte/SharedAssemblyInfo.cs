7720e7ddff9d92befad15d1ac6a7cad557ee6215
fabio <fabio@pdvseven.com.br>
2023-05-08 16:01:45 -0300

    versionamento

--- Fonte/SharedAssemblyInfo.cs
+++ Fonte/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("PDV7")]
 [assembly: AssemblyCopyright("Copyright © PDVSeven 2014-2022")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.24.6.1")]
+[assembly: AssemblyVersion("2.24.7.0")]
 
 /* CheckList antes de gerar Release!
  *    Verificar se os WS2 (Autoatendimento, Saida, PainelMesaComanda) estão apontando para o "." que é para descoberta automática via UDP
@@ -66,6 +66,10 @@ Lista das últimas versões e FIX
 
 ===============================================================================
 ===============================================================================
+
+2.24.7.0 ======================================================================
+    Melhoria: Melhorias na estrutura de projetos e referencias
+    Melhoria: Atualização e Consolidação de versões de pacotes
 
 2.24.6.1 ======================================================================
     Correção: Ajuste na importacao do meio de pagamento do Delivery On! pelo Range
