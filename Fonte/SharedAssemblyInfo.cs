6e2df3b59e855a4ec541bf26baf12303978076e1
tatohonma <milton@pdvseven.com.br>
2025-11-11 16:06:09 -0300

    atualizacao-sdk

--- Fonte/SharedAssemblyInfo.cs
+++ Fonte/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("PDV7")]
 [assembly: AssemblyCopyright("Copyright © PDVSeven 2014-2025")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.25.5.4")]
+[assembly: AssemblyVersion("2.25.6.0")]
 
 /* CheckList antes de gerar Release!
  *    Verificar se os WS2 (Autoatendimento, Saida, PainelMesaComanda) estão apontando para o "." que é para descoberta automática via UDP
@@ -66,6 +66,9 @@ Lista das últimas versões e FIX
 
 ===============================================================================
 ===============================================================================
+
+2.25.6.0 ======================================================================
+    Novo: Integração Autotef Stone com Vale Refeição e PIX
 
 2.25.5.4 ======================================================================
     Novo: Inclusão do campo integração TEF no XML da NFC-e
