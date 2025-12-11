07623c4a39a245ea6c81bdfcd62906f1c9c6b6e7
annathais <annathais17@gmail.com>
2025-12-11 13:37:14 -0300

    fix: correção no calculo da alíquota do icms00 e icms900

--- Fonte/a7D.PDV.Fiscal/NFCe/NFCeVenda.cs
+++ Fonte/a7D.PDV.Fiscal/NFCe/NFCeVenda.cs
@@ -294,12 +294,19 @@ namespace a7D.PDV.Fiscal.NFCe
             {
                 if (imposto.ICMS == null) imposto.ICMS = new ICMS();
 
+                var vBC = vProd;
+                var pICMS = tributacao.ICMS00_pICMS.ToDecimal();
+                
+                var vICMS = Math.Round(vBC * pICMS/ 100m, 2, MidpointRounding.AwayFromZero);
+                
+
                 imposto.ICMS.TipoICMS = new ICMS00()
                 {
                     orig = tributacao.ICMS00_Orig.ToEnum<OrigemMercadoria>(),
                     CST = tributacao.ICMS00_CST.ToEnum<Csticms>(),
                     pICMS = tributacao.ICMS00_pICMS.ToDecimal(),
-                    vBC = vProd
+                    vBC = vProd,
+                    vICMS = vICMS,
                 };
             }
 
@@ -375,12 +382,18 @@ namespace a7D.PDV.Fiscal.NFCe
              && !string.IsNullOrEmpty(tributacao.ICMSSN900_pICMS))
             {
                 if (imposto.ICMS == null) imposto.ICMS = new ICMS();
+                
+                var vBC = vProd;
+                var pICMS = tributacao.ICMSSN900_pICMS.ToDecimal();
+                var vICMS = Math.Round(vBC * pICMS / 100m, 2, MidpointRounding.AwayFromZero);
 
                 imposto.ICMS.TipoICMS = new ICMSSN900()
                 {
                     orig = tributacao.ICMSSN900_Orig.ToEnum<OrigemMercadoria>(),
                     CSOSN = tributacao.ICMSSN900_CSOSN.ToEnum<Csosnicms>(),
                     pICMS = tributacao.ICMSSN900_pICMS.ToDecimal(),
+                    vBC = vBC,
+                    vICMS = vICMS,
                 };
             }
 
