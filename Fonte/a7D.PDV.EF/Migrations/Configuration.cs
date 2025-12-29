f8b016322b52daff33cd4377b768c19a98b0be5f
Joao <58124203+J-NSC@users.noreply.github.com>
2025-12-29 18:09:47 -0300

    fix: correção seed

--- Fonte/a7D.PDV.EF/Migrations/Configuration.cs
+++ Fonte/a7D.PDV.EF/Migrations/Configuration.cs
@@ -107,7 +107,7 @@ namespace a7D.PDV.EF.Migrations
             AjustesVersao2_25_4_3(context, versao);
             AjustesVersao2_25_5_3(context, versao);
             AjusteVersao2_25_6_3(context, versao);
-            Ajuste2_25_7_0(context, versao);
+            Ajuste2_25_8_0(context, versao);
 
             // Limpeza iFood e ERP: Remover futuramente
             if (versao != null && versao.ToVersion() < new Version("2.17.16.6"))
@@ -564,9 +564,9 @@ namespace a7D.PDV.EF.Migrations
         }
         
         
-        private void Ajuste2_25_7_0(pdv7Context context, tbVersao versao)
+        private void Ajuste2_25_8_0(pdv7Context context, tbVersao versao)
         {
-            if (versao == null || versao.ToVersion() < new Version("2.25.7.0"))
+            if (versao == null || versao.ToVersion() < new Version("2.25.8.0"))
             {
                 context.tbTipoSolicitacaoSATs.AddOrUpdate(p => p.IDTipoSolicitacaoSAT,
                     new tbTipoSolicitacaoSAT {IDTipoSolicitacaoSAT = 3 , Nome = "InutilizarVenda"}
